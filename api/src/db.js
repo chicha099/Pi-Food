edbdeb6a75da383a55f8ee76845664f82a54bb0d
chicha099 <tdsserafin@gmail.com>
2021-10-22 22:51:34 -0300

    diet to type

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Types } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Recipe.belongsToMany(Types, { through: "recipe_types" });
+Types.belongsToMany(Recipe, { through: "recipe_types" });
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ api/src/routes/recipes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const axios = require('axios');
+
+const router = Router();
+
+router.get("/", (req, res) => {
+   
+})
+
+module.exports = router;
