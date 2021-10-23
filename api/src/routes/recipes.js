abf9dc6f15e70132299f32341389e26f12cd931b
chicha099 <tdsserafin@gmail.com>
2021-10-22 23:38:56 -0300

    types route ready

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -4,7 +4,10 @@ const axios = require('axios');
 const router = Router();
 
 router.get("/", (req, res) => {
-   
+   axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=8be1fbec844c400db5c85a38e275bd9f&addRecipeInformation=true&number=100")
+   .then(resp =>  {
+       return res.json(resp.data)
+   })
 })
 
 module.exports = router;
