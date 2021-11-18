1ec9ec21de912036136e89154d29d755a0769bb9
chicha099 <tdsserafin@gmail.com>
2021-11-18 13:31:38 -0300

    aaaaaaa

--- api/src/app.js
+++ api/src/app.js
@@ -8,7 +8,9 @@ require('./db.js');
 
 const server = express();
 const cors = require('cors')
+
 server.name = 'API';
+
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
